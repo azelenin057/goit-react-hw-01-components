d55d6e4f49055562542748206b98d2c9b8e45d15
azelenin057 <yazel057@gmail.com>
2023-11-11 01:38:41 +0200

    ex1

---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import css from "./Profile.module.css";
+
+export const Profile = props => {
+    return (
+    <div class={css.profile}>
+        <div class={css.description}>
+            <img
+                src={props.avatar}
+                alt="User avatar"
+                class={css.avatar}
+            />
+            <p class="name">{props.username}</p>
+            <p class="tag">@{props.tag}</p>
+            <p class="location">{props.location}</p>
+        </div>
+
+        <ul className={css.stats}>
+            <li className={css.stats_item}>
+            <span class="label">Followers</span>
+            <span class="quantity">{props.stats.followers}</span>
+            </li>
+            <li className={css.stats_item}>
+            <span class="label">Views</span>
+            <span class="quantity">{props.stats.views}</span>
+            </li>
+            <li className={css.stats_item}>
+            <span class="label">Likes</span>
+            <span class="quantity">{props.stats.likes}</span>
+            </li>
+        </ul>
+    </div>)
+  };
+  
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { Profile } from 'components/Profile';
 import './index.css';
+import user from '../src/user.json';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>);
